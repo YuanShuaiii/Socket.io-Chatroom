b8317a1d4f7afa0ab6d0871175529bdc532a2fd9
YuanShuai <yuanshuai0729@gmail.com>
2016-11-23 10:55:13 +0800

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-server.listen(3000);
+server.listen(3001);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,16 +14,39 @@ app.get('/', function(req, res){
 var arr =[];
 
 io.on('connection', function(socket){
+
+
   // socket.broadcast.emit('broadcast');
   socket.on('name', function (name) {
+      socket.name = name;
      arr.push(name);
      console.log('msg');
      socket.broadcast.emit('broadcast', name+'joined');
      socket.broadcast.emit('arr', arr.join(','));
   });
 
+
   socket.on('chat message', function(msg){
     // console.log(msg);
     io.emit('chat message', msg)
   });
+
+
+
+  socket.on('disconnect', function () {
+      var  delName = socket.name;
+      for (var i = 0; i < arr.length; i++) {
+          var nameTemp = arr[i];
+          if(nameTemp==delName){
+              arr.splice(i,1)
+          }
+
+      }
+      // arr.remove();
+      socket.broadcast.emit('broadcast', socket.name + 'leaved');
+      socket.broadcast.emit('arr', arr.join(','));
+      // socket.broadcast.emit('arr', arr.join(','));
+  })
 });
+
+
